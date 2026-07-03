29d5bef6ccc0750e55cb33ea13f4250eb924993e
void <vinayudn.07@gmail.com>
2026-07-03 14:27:58 +0530

    Time: 0 ms (100%), Space: 21.7 MB (99.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,3 +1,29 @@
+#pragma GCC optimize("Ofast")
+
+#include <iostream>
+#include <vector>
+
+static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void *operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return &BUFFER[pos - size];
+}
+
+void *operator new[](const std::size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void *) noexcept {}
+
+void operator delete[](void *) noexcept {}
+
+void operator delete(void *, std::size_t) noexcept {}
+
+void operator delete[](void *, std::size_t) noexcept {}
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {

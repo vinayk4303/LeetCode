109884ad1248242162ec94768422a270c1abfa72
void <vinayudn.07@gmail.com>
2026-07-06 10:15:22 +0530

    Time: 9 ms (28.66%), Space: 55.1 MB (31.77%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int k = 1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == k){
+                k++;
+            }
+            else if (nums[i] < k){
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+};

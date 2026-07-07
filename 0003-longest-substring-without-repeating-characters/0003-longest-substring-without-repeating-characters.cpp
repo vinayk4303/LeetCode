cfdfee6b46f2c32303ca30c6a38da7d0d3d37662
void <vinayudn.07@gmail.com>
2026-07-07 21:57:18 +0530

    Time: 0 ms (100%), Space: 10 MB (99.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -15,3 +15,22 @@ public:
         return maxi;
     }
 };
+static const bool __boost = []() {
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return std::ios_base::sync_with_stdio(false);
+}();
+const size_t BUFFER_SIZE = 0x3000000;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
+}
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}

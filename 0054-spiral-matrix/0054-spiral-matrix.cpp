8f70a7fdb7605360a4df244cd94d693c5463d17e
void <vinayudn.07@gmail.com>
2026-07-08 13:30:35 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.95%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -36,3 +36,23 @@ public:
         return res;
     }
 };
+static const bool __boost = []() {
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return std::ios_base::sync_with_stdio(false);
+	}();
+
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+void* operator new(size_t size) {
+	constexpr std::size_t alignment = alignof(std::max_align_t);
+	size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+	size_t total_size = size + padding;
+	char* aligned_ptr = &buffer[buffer_pos + padding];
+	buffer_pos += total_size;
+	return aligned_ptr;
+}
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}

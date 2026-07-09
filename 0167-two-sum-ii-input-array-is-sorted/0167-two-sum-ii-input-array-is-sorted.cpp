526291a46ff0c11a50637e2ba020b6666c7eef88
void <vinayudn.07@gmail.com>
2026-07-10 00:09:17 +0530

    Time: 0 ms (100%), Space: 18.8 MB (99.96%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -18,3 +18,24 @@ public:
         return{};
     }
 };
+static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void *operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const std::size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void *) noexcept {}
+
+void operator delete[](void *) noexcept {}
+
+void operator delete(void *, std::size_t) noexcept {}
+
+void operator delete[](void *, std::size_t) noexcept {}

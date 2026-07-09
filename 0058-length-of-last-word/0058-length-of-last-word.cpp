00007f3516ae89db5ea26171fc6e21e4670564f0
void <vinayudn.07@gmail.com>
2026-07-09 23:31:37 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.26%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        for (int i= s.size()-1; i >=0 ; i-- ){
+            if(s[i] != ' '){
+                len++;
+            }
+            else if (len > 0){
+                break;
+            }
+        }
+        return len;
+    }
+};
